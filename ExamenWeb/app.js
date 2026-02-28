43a638a7168abea3ab7b7a512014d400fd05dcc5
zurira <20243ds036@utez.edu.mx>
2026-02-27 19:44:10 -0600

    Inicio de app.js y referenciación de elementos

---
+++ ExamenWeb/app.js
@@ -0,0 +1,4 @@
+const inpBuscar = document.getElementById("inpBuscar");
+const dateTo = document.getElementById("dateTo");
+const dateFrom = document.getElementById("dateFrom");
+const container = document.getElementById("container");
